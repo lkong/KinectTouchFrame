55eca5b3f4c5f1417e2a79ec823a889afa0fcd95
lkong <kongliang87@gmail.com>
2011-03-20 11:26:43 -0500

    hand pos

--- KinectTouchFrame/MainWindow.cs
+++ KinectTouchFrame/MainWindow.cs
@@ -349,7 +349,8 @@ namespace UserTracker.net
                         nearestUser = user;
                     if (Math.Abs(X - TransformToScreenPos(RightHand, width, height).X) < BelongingThresold && Math.Abs(Y - TransformToScreenPos(RightHand, width, height).Y) < BelongingThresold)
                         nearestUser = user;
-                    Console.Out.WriteLine("Get Touch in " + X + " , " + Y + " Selected Region " + SelectedMinX + " to " + SelectedMaxX + " , " + SelectedMinY + " to " + SelectedMaxY + " Transform to " + TransformToScreenPos(RightHand, width, height).X + " , " + TransformToScreenPos(RightHand, width, height).Y); 
+                    Console.Out.WriteLine("Get Touch in " + X + " , " + Y +" Transform to " + TransformToScreenPos(RightHand, width, height).X + " , " + TransformToScreenPos(RightHand, width, height).Y);
+                    Console.Out.WriteLine("Lefthand in "+LeftHand.X+" , "+LeftHand.Y+" RightHand in "+RightHand.X+" , "+RightHand.Y + " Selected Region " + SelectedMinX + " to " + SelectedMaxX + " , " + SelectedMinY + " to " + SelectedMaxY);
                     //e.GetTouchPoint().
                 }
 
