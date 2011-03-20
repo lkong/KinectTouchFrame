06273c3877803f42c895f22058e7242fb5eac811
lkong <kongliang87@gmail.com>
2011-03-19 21:42:22 -0500

    show hand

--- KinectTouchFrame/SurfaceWindow1.xaml.cs
+++ KinectTouchFrame/SurfaceWindow1.xaml.cs
@@ -39,5 +39,30 @@ namespace KinectTouchFrame
             Console.Out.WriteLine(watchingWindow.GetUser(X, Y, (int)this.Width, (int)this.Height));
         }
         private KinectWindow watchingWindow;
+
+        private void HandRender()
+        {
+            Dictionary<String, System.Drawing.Point> Hands = watchingWindow.getHands((int)this.Width, (int)this.Height);
+            foreach (KeyValuePair<String, System.Drawing.Point> pair in Hands)
+            {
+                System.Drawing.Point HandPoint = pair.Value;
+                String HandString = pair.Key;
+                if (HandPoint.X!=0 && HandPoint.Y!=0)
+                    if (HandPoint.X < (int)this.Width && HandPoint.Y < (int)this.Height)
+                    {
+                        Ellipse HandRange = new Ellipse();
+                        HandRange.Fill = System.Windows.Media.Brushes.DarkBlue;
+                        HandRange.Width = 100;
+                        HandRange.Height = 100;
+                        RotateTransform pos = new RotateTransform();
+                        pos.CenterX = HandPoint.X;
+                        pos.CenterY = HandPoint.Y;
+                        HandRange.RenderTransform = pos;
+                       // HandRange.
+                        DrawingGrid.Children.Add(HandRange);
+                    }
+            }
+        
+        }
     }
 }
